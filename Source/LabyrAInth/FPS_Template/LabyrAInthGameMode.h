04ee7a8f683b68f49781b5aa208c8ced4aa4d204
PieMH <pdondiw@gmail.com>
2024-02-17 17:45:24 +0100

    Revert "Merge pull request #1 from ringmaster-lenovo/VR_Template"
    This reverts commit 373496befa286ee1365eff40130b35745cdbf8f8, reversing
    changes made to 6f7414042ddc1b9eb51f91e1c8a96472712f97cf.

---
+++ Source/LabyrAInth/FPS_Template/LabyrAInthGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LabyrAInthGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALabyrAInthGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALabyrAInthGameMode();
+};
+
+
+
