6da6e1cd5f7c2e3806ec6354c9b238c2a8769b12
xmaswa <85439804+xmaswa@users.noreply.github.com>
2024-04-28 17:40:52 +0200

    Projectile

--- Source/LabyrAInthVR/Projectiles/Projectile.h
+++ Source/LabyrAInthVR/Projectiles/Projectile.h
@@ -38,7 +38,7 @@ private:
 	UPROPERTY(EditAnywhere)
 	UProjectileMovementComponent* ProjectileMovementComponent;
 	
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
 	UStaticMeshComponent* ProjectileMesh;
 	
 	UPROPERTY(EditAnywhere)
